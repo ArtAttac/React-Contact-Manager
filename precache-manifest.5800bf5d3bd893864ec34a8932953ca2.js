f1bea3ad80453c034f86469ea9b9cf925824fbf8
Arthur I <sovietbulldozer@gmail.com>
2019-04-26 20:03:19 -0400

    Updates

---
+++ precache-manifest.5800bf5d3bd893864ec34a8932953ca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6eef73c19750ebb26f8d",
+    "url": "/React-Contact-Manager/static/css/main.72ce61af.chunk.css"
+  },
+  {
+    "revision": "6eef73c19750ebb26f8d",
+    "url": "/React-Contact-Manager/static/js/main.732c0b7c.chunk.js"
+  },
+  {
+    "revision": "401c84a7cd0c5005f318",
+    "url": "/React-Contact-Manager/static/js/runtime~main.5648dd7e.js"
+  },
+  {
+    "revision": "237cea77452ee861f8c8",
+    "url": "/React-Contact-Manager/static/css/2.e8f1c4c4.chunk.css"
+  },
+  {
+    "revision": "237cea77452ee861f8c8",
+    "url": "/React-Contact-Manager/static/js/2.4ba8ee9f.chunk.js"
+  },
+  {
+    "revision": "fe4f7a5b2fea6bf6771fe7ef6c497807",
+    "url": "/React-Contact-Manager/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-contact-manager/precache-manifest.ff5a6e1e8d28e5ad35404180ec7647e8.js"
+  "/React-Contact-Manager/precache-manifest.5800bf5d3bd893864ec34a8932953ca2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-contact-manager/index.html", {
+workbox.routing.registerNavigationRoute("/React-Contact-Manager/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
